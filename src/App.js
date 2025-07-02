d728ca411133a4be7659e2bc4ab26e7b6346de73
streetbishop <streetbishop@streetbishop>
2025-07-02 13:28:56 +0100

    FIX: Changed the Header title of the solution

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Retirement Calculator</h1>
+        <h1>Financial Freedom Calculator</h1>
       </header>
       <main className="calculator-container">
         <section className="details-panel">
